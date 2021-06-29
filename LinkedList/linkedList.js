2b2146ae072d974b99cf5dd85e55db8c982bc2a5
unknown <indalkumar56@gmail.com>
2021-06-29 18:12:30 +0530

    ds algo

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,10 @@
+//Print the Middle of a given linked list
+//Delete middle of linked list
+//Remove duplicate elements from sorted linked list
+//Remove loop in linked list
+//Find nth node from the end of linked list
+//Function to check if a singly linked list is a palindrome
+//Delete last occurrence of an item from linked list
+///Rotate a linked list.
+//Write a function to delete a linked list.
+//Write a function to reverse the nodes of a linked list.
